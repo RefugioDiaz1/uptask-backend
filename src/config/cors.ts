d02fc8f1d9499d87c06ac358aad0b32d44a943dd
Refugio Diaz <refugiodelaodiaz@gmail.com>
2026-04-18 17:57:28 -0600

    Cambio en cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,25 +1,22 @@
 import {CorsOptions} from 'cors'
 
-export const corsConfig : CorsOptions= {
-    origin: function(origin, callback)
-    {
-        const whitelist = [process.env.FRONTEND_URL]
-        
-        // if (!origin) {
-        //     // Permite acceso directo desde navegador o herramientas como Postman
-        //     return callback(null, true)
-        // }
+export const corsConfig: CorsOptions = {
+  origin: function (origin, callback) {
 
-        if(process.argv[2] === '--api')
-        {
-            whitelist.push(undefined)
-        }
+    const whitelist = [
+      process.env.FRONTEND_URL
+    ]
 
-        if(whitelist.includes(origin))
-        {
-            callback(null, true)
-        }else{
-            callback(new Error('Error de Cors'))
-        }
+    if (!origin) return callback(null, true)
+
+    const isAllowed = whitelist.some(url => 
+      url && origin.includes(url)
+    )
+
+    if (isAllowed) {
+      return callback(null, true)
     }
+
+    return callback(new Error('Error de Cors'))
+  }
 }
